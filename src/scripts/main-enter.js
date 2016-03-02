89e1029d2ae4a8b2bd1fc8b13c3d48694dd50edd
Matthew Caldwell <caldwellysr@gmail.com>
2016-03-01 23:22:35 -0500

    working tilemap with spawn point and collisions

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,4 +1,32 @@
 "use strict";
 
 module.exports = function(game) { // eslint-disable-line no-unused-vars
+	var file = require("../data/tilemap.json");
+	var tilemap = 1;
+	var player = 2;
+	var tilemap_component = game.entities.get(tilemap, "tilemap");
+	var collider, layer, object;
+	for (var i = 0; i < file.layers.length; i++) {
+		layer = file.layers[i];
+		if (layer.name == tilemap_component.layer) {
+			tilemap_component.layer_index = i;
+		}
+		if (layer.name == "Collisions") {
+			for (var j = 0; j < layer.objects.length; j++) {
+				object = layer.objects[j];
+				collider = game.instantiatePrefab("collision");
+				game.entities.set(collider, "size", { "width": object.width, "height": object.height });
+				game.entities.set(collider, "position", { "x": object.x, "y": object.y });
+			}
+		}
+		if (layer.name == "Spawn") {
+			object = layer.objects[0];
+			game.entities.set(player, "position", { "x": object.x, "y": object.y });
+		}
+	}
+	var map_size = {
+		"width": file.width * file.tilewidth,
+		"height": file.height * file.tileheight
+	};
+	game.entities.set(tilemap, "size", map_size);
 };
--- src/systems/renderer/sample-renderer-system.js
+++ src/systems/renderer/sample-renderer-system.js
@@ -1,11 +1,23 @@
 "use strict";
 
+/**
+ * When looping through layer data index
+ * var x = (index % width) * tileWidth;
+ * var y = Math.floor(index/height) * tileHeight;
+ */
+
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
-	game.entities.registerSearch("sampleRendererSystem", ["camera", "position", "size"]);
+	var file = require("../../data/tilemap.json");
+	var x, y, image, image_index, i, index;
+	game.entities.registerSearch("sampleRendererSystem", ["tilemap", "position", "size"]);
 	ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
-		var position = game.entities.get(entity, "position");
-		var size = game.entities.get(entity, "size");
-		context.fillStyle = "black";
-		context.fillRect(position.x, position.y, size.width, size.height);
+		index = game.entities.get(entity, "tilemap").layer_index;
+		for (i = 0; i < file.layers[index].data.length; i++) {
+			x = (i % file.width) * file.tilewidth;
+			y = Math.floor(i / file.height) * file.tileheight;
+			image_index = file.layers[index].data[i] - 1;
+			image = game.images.get(file.tilesets[image_index].image);
+			context.drawImage(image,x,y,file.tilewidth,file.tileheight);
+		}
 	}, "sampleRendererSystem");
 };
