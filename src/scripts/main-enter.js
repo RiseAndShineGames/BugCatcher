cfdda5fa4f3c4b83022288cf924f085fb3fc9a9c
Matthew Caldwell <caldwellysr@gmail.com>
2016-03-02 07:40:38 -0500

    render all tilelayers as prefabs instead of searching for a specific layer name

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -2,14 +2,23 @@
 
 module.exports = function(game) { // eslint-disable-line no-unused-vars
 	var file = require("../data/tilemap.json");
-	var tilemap = 1;
+	var tilemap;
+	var background_layer = 9999;
 	var player = 2;
-	var tilemap_component = game.entities.get(tilemap, "tilemap");
+	var tilemap_component, tilelayer_position;
 	var collider, layer, object;
 	for (var i = 0; i < file.layers.length; i++) {
 		layer = file.layers[i];
-		if (layer.name == tilemap_component.layer) {
+		if (layer.type == "tilelayer") {
+			tilemap = game.instantiatePrefab("tilelayer");
+			tilemap_component = game.entities.get(tilemap, "tilemap");
+			tilelayer_position = game.entities.get(tilemap, "position");
+			tilelayer_position.z = i;
 			tilemap_component.layer_index = i;
+			tilemap_component.layer = layer.name;
+			if (tilemap < background_layer) {
+				background_layer = tilemap;
+			}
 		}
 		if (layer.name == "Collisions") {
 			for (var j = 0; j < layer.objects.length; j++) {
@@ -29,5 +38,5 @@ module.exports = function(game) { // eslint-disable-line no-unused-vars
 		"height": file.height * file.tileheight
 	};
 	game.entities.set(tilemap, "size", map_size);
-	game.entities.set(player, "constrainPosition", { "id": 1 });
+	game.entities.set(player, "constrainPosition", { "id": background_layer });
 };
