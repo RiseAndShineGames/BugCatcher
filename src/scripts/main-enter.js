5d80db3637871bad5a8c4b3cba393c81c8b73b58
Matthew Caldwell <caldwellysr@gmail.com>
2016-03-02 21:18:24 -0500

    refactor object layers

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -2,44 +2,35 @@
 
 module.exports = function(game) { // eslint-disable-line no-unused-vars
 	var file = require("../data/tilemap.json");
-	var tile, image, image_index, x, y;
+	var tile, image, image_index, tile_pos;
 	var player = 1, container = 2;
 	var collider, layer, object;
 	for (var i = 0; i < file.layers.length; i++) {
 		layer = file.layers[i];
 		if (layer.type == "tilelayer") {
-			/*
-			tilemap = game.instantiatePrefab("tilelayer");
-			tilemap_component = game.entities.get(tilemap, "tilemap");
-			tilelayer_position = game.entities.get(tilemap, "position");
-			tilelayer_position.z = i;
-			tilemap_component.layer_index = i;
-			tilemap_component.layer = layer.name;
-			if (tilemap < background_layer) {
-				background_layer = tilemap;
-			}
-			*/
 			for (j = 0; j < layer.data.length; j++) {
 				tile = game.instantiatePrefab("tile");
 				image = game.entities.get(tile, "image");
-				x = (j % file.width) * file.tilewidth;
-				y = Math.floor(j / file.height) * file.tileheight;
+				tile_pos = game.entities.get(tile, "position");
+				tile_pos.x = (j % file.width) * file.tilewidth;
+				tile_pos.y = Math.floor(j / file.height) * file.tileheight;
+				tile_pos.z = (layer.name == "Background") ? -1 : 2;
 				image_index = layer.data[j] - 1;
 				image.name = file.tilesets[image_index].image;
-				game.entities.set(tile, "position", { "x": x, "y": y });
 			}
 		}
-		if (layer.name == "Collisions") {
+		if (layer.type == "objectgroup") {
 			for (var j = 0; j < layer.objects.length; j++) {
-				object = layer.objects[j];
-				collider = game.instantiatePrefab("collision");
-				game.entities.set(collider, "size", { "width": object.width, "height": object.height });
-				game.entities.set(collider, "position", { "x": object.x, "y": object.y });
+				if (layer.properties.Collidable) {
+					object = layer.objects[j];
+					collider = game.instantiatePrefab("collision");
+					game.entities.set(collider, "size", { "width": object.width, "height": object.height });
+					game.entities.set(collider, "position", { "x": object.x, "y": object.y });
+				} else if (layer.properties.spawnPoint) {
+					object = layer.objects[0];
+					game.entities.set(player, "position", { "x": object.x, "y": object.y });
+				}
 			}
-		}
-		if (layer.name == "Spawn") {
-			object = layer.objects[0];
-			game.entities.set(player, "position", { "x": object.x, "y": object.y });
 		}
 	}
 	var map_size = {
