3659c35a11e91ec1b09bc75b6fc17d1fec2122b9
Matthew Caldwell <caldwellysr@gmail.com>
2016-03-04 00:23:53 -0500

    this isn't working

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,10 +1,56 @@
 "use strict";
 
+/*
+function clone(obj) {
+	if (obj === undefined) {
+		return undefined;
+	}
+	return JSON.parse(JSON.stringify(obj));
+}
+function splitFilmStripAnimations(animations) {
+	Object.keys(animations).forEach(function(key) {
+		var firstFrame = animations[key][0];
+		if (firstFrame.filmstripFrames) {
+			splitFilmStripAnimation(animations, key);
+		}
+	});
+}
+function splitFilmStripAnimation(animations, key) {
+	var firstFrame = animations[key][0];
+	if (firstFrame.properties.image.sourceWidth % firstFrame.filmstripFrames != 0) {
+		console.warn("The \"" + key + "\" animation is " + firstFrame.properties.image.sourceWidth + " pixels wide and that is is not evenly divisible by " + firstFrame.filmstripFrames + " frames.");
+	}
+	for (var i = 0; i < firstFrame.filmstripFrames; i++) {
+		var frameWidth = firstFrame.properties.image.sourceWidth / firstFrame.filmstripFrames;
+		var newFrame = clone(firstFrame);
+		newFrame.properties.image.sourceX = frameWidth * i;
+		newFrame.properties.image.sourceWidth = frameWidth;
+		animations[key].push(newFrame);
+	}
+	animations[key].splice(0,1);
+}
+*/
+
+/**
+ * Tilesets
+ * What I know:
+ *		imagewidth
+ *		imageheight
+ *		tilewidth
+ *		tileheight
+ * cols = imagewidth / tilewidth;
+ * rows = imageheight / tileheight;
+ * var x = (i % cols) * tilewidth;
+ * var y = Math.floor(i/rows) * tileheight;
+ */
+
 module.exports = function(game) { // eslint-disable-line no-unused-vars
 	var file = require("../data/tilemap.json");
 	var tile, image, image_index, tile_pos;
 	var player = 1, container = 2;
 	var collider, layer, object;
+	var cols = file.tilesets[0].imagewidth / file.tilesets[0].tilewidth;
+	var rows = file.tilesets[0].imageheight / file.tilesets[0].tileheight;
 	for (var i = 0; i < file.layers.length; i++) {
 		layer = file.layers[i];
 		if (layer.type == "tilelayer") {
@@ -17,7 +63,11 @@ module.exports = function(game) { // eslint-disable-line no-unused-vars
 					tile_pos.x = (j % file.width) * file.tilewidth;
 					tile_pos.y = Math.floor(j / file.height) * file.tileheight;
 					tile_pos.z = (layer.properties.Background == "True") ? -1 : 2;
-					image.name = file.tilesets[image_index].image;
+					image.name = file.tilesets[0].image;
+					image.sourceWidth = file.tilesets[0].tilewidth;
+					image.sourceHeight = file.tilesets[0].tileheight;
+					image.sourceX = (image_index % cols) * file.tilesets[0].tilewidth;
+					image.sourceY = Math.floor(image_index / rows) * file.tilesets[0].tileheight;
 				}
 			}
 		}
