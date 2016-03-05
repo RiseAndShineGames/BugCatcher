347b8aeaa75cdc44aaf4a349733b0841892e69a1
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2016-03-04 23:14:02 -0500

    comment enter script code

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,35 +1,5 @@
 "use strict";
-
-/*
-function clone(obj) {
-	if (obj === undefined) {
-		return undefined;
-	}
-	return JSON.parse(JSON.stringify(obj));
-}
-function splitFilmStripAnimations(animations) {
-	Object.keys(animations).forEach(function(key) {
-		var firstFrame = animations[key][0];
-		if (firstFrame.filmstripFrames) {
-			splitFilmStripAnimation(animations, key);
-		}
-	});
-}
-function splitFilmStripAnimation(animations, key) {
-	var firstFrame = animations[key][0];
-	if (firstFrame.properties.image.sourceWidth % firstFrame.filmstripFrames != 0) {
-		console.warn("The \"" + key + "\" animation is " + firstFrame.properties.image.sourceWidth + " pixels wide and that is is not evenly divisible by " + firstFrame.filmstripFrames + " frames.");
-	}
-	for (var i = 0; i < firstFrame.filmstripFrames; i++) {
-		var frameWidth = firstFrame.properties.image.sourceWidth / firstFrame.filmstripFrames;
-		var newFrame = clone(firstFrame);
-		newFrame.properties.image.sourceX = frameWidth * i;
-		newFrame.properties.image.sourceWidth = frameWidth;
-		animations[key].push(newFrame);
-	}
-	animations[key].splice(0,1);
-}
-*/
+var prefabLibrary = require("prefabLib");
 
 /**
  * Tilesets
@@ -46,22 +16,36 @@ function splitFilmStripAnimation(animations, key) {
 
 module.exports = function(game) { // eslint-disable-line no-unused-vars
 	var file = require("../data/tilemap.json");
+
+	// Tile layer variables
 	var tile, image, image_index, tile_pos;
-	var player = 1, container = 2;
 	var collider, layer, object;
 	var cols = file.tilesets[0].imagewidth / file.tilesets[0].tilewidth;
+	
 	for (var i = 0; i < file.layers.length; i++) {
 		layer = file.layers[i];
+
+		// Render Tile Layers
 		if (layer.type == "tilelayer") {
 			for (j = 0; j < layer.data.length; j++) {
+
+				// Tiled gives 1-based indexes. Subtract one for position math
 				image_index = layer.data[j] - 1;
 				if (image_index >= 0) {
+
+					// Create tile and get properties
 					tile = game.instantiatePrefab("tile");
 					image = game.entities.get(tile, "image");
 					tile_pos = game.entities.get(tile, "position");
+
+					// Position based on index
 					tile_pos.x = (j % file.width) * file.tilewidth;
 					tile_pos.y = Math.floor(j / file.height) * file.tileheight;
+
+					// Character position z: 1 so anything without background true will layer over player
 					tile_pos.z = (layer.properties.Background == "True") ? -1 : 2;
+
+					// Select which "tile" of the tileset image to render
 					image.name = file.tilesets[0].image;
 					image.sourceWidth = file.tilesets[0].tilewidth;
 					image.sourceHeight = file.tilesets[0].tileheight;
@@ -69,29 +53,38 @@ module.exports = function(game) { // eslint-disable-line no-unused-vars
 					image.destinationHeight = file.tilesets[0].tileheight;
 					image.sourceX = (image_index % cols) * file.tilesets[0].tilewidth;
 					image.sourceY = Math.floor(image_index / cols) * file.tilesets[0].tileheight;
-					console.log(image);
+					
 				}
 			}
 		}
+
+		// Loop object layer for collisions and spawn point
+		// TODO Bug spawn chance triggers, doors, switches (buttons), warp zones, NPC Spawn points
 		if (layer.type == "objectgroup") {
 			for (var j = 0; j < layer.objects.length; j++) {
 				object = layer.objects[j];
+
+				// Collision box prefabs
 				if (object.properties.Collidable == "True") {
 					collider = game.instantiatePrefab("collision");
 					game.entities.set(collider, "size", { "width": object.width, "height": object.height });
 					game.entities.set(collider, "position", { "x": object.x, "y": object.y });
 				}
+
+				// Spawn points
 				if (object.properties.Spawn == "True") {
 					game.entities.set(player, "position", { "x": object.x, "y": object.y });
 				}
 			}
 		}
 	}
+
+	// Define map bounding box for contrain position
+	var player = 1, container = 2;
 	var map_size = {
 		"width": file.width * file.tilewidth,
 		"height": file.height * file.tileheight
 	};
 	game.entities.set(container, "size", map_size);
 	game.entities.set(player, "constrainPosition", { "id": container });
-	console.log(game.entities);
 };
