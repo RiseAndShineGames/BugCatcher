9b5ee43716531f663d4aee202a8296ee9142a0ee
Matthew Caldwell <caldwellysr@gmail.com>
2016-03-05 01:34:10 -0500

    add support for multiple tilesets

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,36 +1,5 @@
 "use strict";
 
-/*
-function clone(obj) {
-	if (obj === undefined) {
-		return undefined;
-	}
-	return JSON.parse(JSON.stringify(obj));
-}
-function splitFilmStripAnimations(animations) {
-	Object.keys(animations).forEach(function(key) {
-		var firstFrame = animations[key][0];
-		if (firstFrame.filmstripFrames) {
-			splitFilmStripAnimation(animations, key);
-		}
-	});
-}
-function splitFilmStripAnimation(animations, key) {
-	var firstFrame = animations[key][0];
-	if (firstFrame.properties.image.sourceWidth % firstFrame.filmstripFrames != 0) {
-		console.warn("The \"" + key + "\" animation is " + firstFrame.properties.image.sourceWidth + " pixels wide and that is is not evenly divisible by " + firstFrame.filmstripFrames + " frames.");
-	}
-	for (var i = 0; i < firstFrame.filmstripFrames; i++) {
-		var frameWidth = firstFrame.properties.image.sourceWidth / firstFrame.filmstripFrames;
-		var newFrame = clone(firstFrame);
-		newFrame.properties.image.sourceX = frameWidth * i;
-		newFrame.properties.image.sourceWidth = frameWidth;
-		animations[key].push(newFrame);
-	}
-	animations[key].splice(0,1);
-}
-*/
-
 /**
  * Tilesets
  * What I know:
@@ -46,15 +15,20 @@ function splitFilmStripAnimation(animations, key) {
 
 module.exports = function(game) { // eslint-disable-line no-unused-vars
 	var file = require("../data/tilemap.json");
-	var tile, image, image_index, tile_pos;
+	var tile, image, image_index, tile_pos, cols, tileset_index = 0;
 	var player = 1, container = 2;
 	var collider, layer, object;
-	var cols = file.tilesets[0].imagewidth / file.tilesets[0].tilewidth;
 	for (var i = 0; i < file.layers.length; i++) {
 		layer = file.layers[i];
 		if (layer.type == "tilelayer") {
 			for (j = 0; j < layer.data.length; j++) {
-				image_index = layer.data[j] - 1;
+				for (var k = 0; k < file.tilesets.length; k++) {
+					if (layer.data[j] > file.tilesets[k].firstgid) {
+						tileset_index = k;
+					}
+				}
+				cols = file.tilesets[tileset_index].imagewidth / file.tilesets[tileset_index].tilewidth;
+				image_index = layer.data[j] - file.tilesets[tileset_index].firstgid;
 				if (image_index >= 0) {
 					tile = game.instantiatePrefab("tile");
 					image = game.entities.get(tile, "image");
@@ -62,14 +36,13 @@ module.exports = function(game) { // eslint-disable-line no-unused-vars
 					tile_pos.x = (j % file.width) * file.tilewidth;
 					tile_pos.y = Math.floor(j / file.height) * file.tileheight;
 					tile_pos.z = (layer.properties.Background == "True") ? -1 : 2;
-					image.name = file.tilesets[0].image;
-					image.sourceWidth = file.tilesets[0].tilewidth;
-					image.sourceHeight = file.tilesets[0].tileheight;
-					image.destinationWidth = file.tilesets[0].tilewidth;
-					image.destinationHeight = file.tilesets[0].tileheight;
-					image.sourceX = (image_index % cols) * file.tilesets[0].tilewidth;
-					image.sourceY = Math.floor(image_index / cols) * file.tilesets[0].tileheight;
-					console.log(image);
+					image.name = file.tilesets[tileset_index].image;
+					image.sourceX = (image_index % cols) * file.tilesets[tileset_index].tilewidth;
+					image.sourceY = Math.floor(image_index / cols) * file.tilesets[tileset_index].tileheight;
+					image.sourceWidth = file.tilesets[tileset_index].tilewidth;
+					image.sourceHeight = file.tilesets[tileset_index].tileheight;
+					image.destinationWidth = file.tilesets[tileset_index].tilewidth;
+					image.destinationHeight = file.tilesets[tileset_index].tileheight;
 				}
 			}
 		}
@@ -93,5 +66,4 @@ module.exports = function(game) { // eslint-disable-line no-unused-vars
 	};
 	game.entities.set(container, "size", map_size);
 	game.entities.set(player, "constrainPosition", { "id": container });
-	console.log(game.entities);
 };
